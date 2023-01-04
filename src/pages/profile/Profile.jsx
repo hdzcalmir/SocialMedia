e13577f9d8e7574e7f371adff1cbb0830d9df23c
Almir Hodžić <hdzcalmir@gmail.com>
2023-01-04 10:40:42 +0100

    ui done

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,64 @@
 import './profile.scss';
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts"
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className="profile">
+      <div className="images">
+        <img src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' className='cover' />
+        <img src="https://images.pexels.com/photos/14686959/pexels-photo-14686959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' className='profilePic' />
+      </div> 
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            {/* mozemo koristit fontSize kao u primjerima ispod za velicinu mui ikona */}
+          <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Nermina Halilcevic</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon/>
+                <span>BiH</span>
+              </div>
+              <div className="item">
+                <LanguageIcon/>
+                <span>almirhodzic.dev</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon/>
+            <MoreVertIcon/>
+          </div>
+        </div>
+        <Posts/>
+
+      </div>
+    </div>
   )
 }
