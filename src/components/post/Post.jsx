426f57c29ac08a666fd9ab4accf2de2f9f11766a
Almir Hodžić <hdzcalmir@gmail.com>
2023-01-22 14:19:46 +0100

    like functionality

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,27 +5,65 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import Comments from '../comments/Comments';
 import moment from 'moment';
+import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { makeRequest } from '../../axios';
+import { AuthContext } from '../../context/authContext';
 
 export const Post = ({post}) => {
 
 
   const [commentOpen, setCommentOpen] = useState(false);
 
-  // temporary
-  const [liked, setLiked] = useState(false);
-  const [likes, setLikes] = useState(99);
+  const {currentUser} = useContext(AuthContext);
 
-  const handleLike = () => { 
-    setLiked(!liked);
-    if(liked) {
-    setLikes(likes - 1);
-    }
-    else {
-      setLikes(likes + 1)
-    }
+  const {isLoading, error, data} = useQuery(["likes", post.id], () =>
+  makeRequest.get("/likes?postId=" + post.id).then((res)=> {
+    return res.data;
+  })
+  );
+
+  // ispisat ce nam prazne arrays jer nemamo nijedan lajk
+// console.log(data);
+
+  // temporary
+  // const [liked, setLiked] = useState(false);
+  // const [likes, setLikes] = useState(99);
+
+  // const handleLike = () => { 
+  //   setLiked(!liked);
+  //   if(liked) {
+  //   setLikes(likes - 1);
+  //   }
+  //   else {
+  //     setLikes(likes + 1)
+  //   }
+  //  }
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation((liked) => {
+    // ukoliko smo lajkovali izvrsit ce se post metoda iz likes controllera
+    if(liked) return makeRequest.delete('/likes?postId=' + post.id);
+    // a ukoliko smo izbrisali like izvrsit ce se delete metoda iz likes
+    return makeRequest.post("/likes", {postId: post.id});
+    // uzimamo post.id kojeg dobivamo u funkciju Post kao argument
+  },
+  {
+    onSuccess: () => { 
+      // invalidate and refatch
+      queryClient.invalidateQueries(["likes"])
+     },
+  }
+  );
+
+
+  const handleLike = ()=> { 
+    // ovo nam vraca true ili false, ovisno od toga da li smo lajkovali post ili ne, odnosno da li je nas id u lajkovima tog posta
+    mutation.mutate(data.includes(currentUser.id)); // ovo saljemo kao argument mutate funkciji, koja to prima u vidu parametra
+    // liked, a on je true ukoliko smo lajkovali i false ukoliko nisamo
    }
 
 
@@ -52,8 +90,11 @@ export const Post = ({post}) => {
       </div>
       <div className="btns">
         <div className="btn">
-        {liked ? <FavoriteOutlinedIcon onClick={handleLike}/> : <FavoriteBorderOutlinedIcon onClick={handleLike}/>}
-        <span>{likes}</span>
+        {isLoading ? "učitava se" : data.includes(currentUser.id) ? 
+        <FavoriteOutlinedIcon style={{color:"red"}} onClick={handleLike}/>: 
+        <FavoriteBorderOutlinedIcon onClick={handleLike} />}
+        {/* <span>{likes}</span> */}
+        <span>{isLoading ? "učitava se" : data.length}</span>
         </div>
         <div className="btn">
         <TextsmsOutlinedIcon onClick={() => setCommentOpen(!commentOpen)}/>
